74e9f4e7b3c97e9aadd1c37d7514f93d05761806
wmn876-sudo <60634641+wmn876-sudo@users.noreply.github.com>
2022-07-06 05:48:05 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,16 +7,6 @@ public class App
 {
 
     public long add(long first, long second) {
-        int a;
-        int b;
-        int c;
-        int d;
-        int e;
-        int f;
-        int g;
-        int h;
-        int i;
-        int j;
         return first + second;
     }
 
